6ab43689d5f3820d4d23226783a8ea42f51be73e
demon <demon@brightstar>
2010-04-11 17:32:33 -0300

    Otro javadoc
    Signed-off-by: demon <demon@brightstar>

--- src/ar/edu/utn/frba/tadp/atencionmedica/asignaciones/AsignacionObserver.java
+++ src/ar/edu/utn/frba/tadp/atencionmedica/asignaciones/AsignacionObserver.java
@@ -5,6 +5,11 @@ import ar.edu.utn.frba.tadp.atencionmedica.unidades.UnidadMedica;
 
 public interface AsignacionObserver {
 
+	/**
+	 * Evento que se dispara cuando se ha asignado una unidad medica a un paciente
+	 * @param unidadMedica
+	 * @param paciente
+	 */
 	void notificar(UnidadMedica unidadMedica, Paciente paciente);
 
 }
