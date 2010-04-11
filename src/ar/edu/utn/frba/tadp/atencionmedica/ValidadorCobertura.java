c614b03b1f2ce2822505a964b8353c34f2a563b0
demon <demon@brightstar>
2010-04-11 17:31:11 -0300

    Agregado javadoc, yeah cambie el ultimo commit?
    Signed-off-by: demon <demon@brightstar>

--- src/ar/edu/utn/frba/tadp/atencionmedica/ValidadorCobertura.java
+++ src/ar/edu/utn/frba/tadp/atencionmedica/ValidadorCobertura.java
@@ -2,6 +2,12 @@ package ar.edu.utn.frba.tadp.atencionmedica;
 
 public interface ValidadorCobertura {
 
+	/**
+	 * Determinar si un paciente posee o no cobertura.
+	 * @param paciente
+	 * @return
+	 * @exception Siempre puede fallar :)
+	 */
 	public boolean cubrePaciente(Paciente paciente);
 
 }
